0f82fecf52528a8890b26e78d97aeb5ffae0bdc0
Robert Gama <robert.gamma@gmail.com>
2018-12-17 12:31:01 -0200

    Updates

---
+++ precache-manifest.6d72a688e48ece75baf779cc11e98298.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "1b7e853e01d73a6cc24d",
+    "url": "/myreads/static/js/runtime~main.1b7e853e.js"
+  },
+  {
+    "revision": "2273b312928d061d579c",
+    "url": "/myreads/static/js/main.2273b312.chunk.js"
+  },
+  {
+    "revision": "3a013310b17f105f5b02",
+    "url": "/myreads/static/js/1.3a013310.chunk.js"
+  },
+  {
+    "revision": "2273b312928d061d579c",
+    "url": "/myreads/static/css/main.80b0cc20.chunk.css"
+  },
+  {
+    "revision": "6570379f90a9f5a26562f47d0d8f349e",
+    "url": "/myreads/index.html"
+  }
+];
